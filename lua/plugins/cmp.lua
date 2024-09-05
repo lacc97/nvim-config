f5fbd26cec71e5b3a6991bba9caf9267f32f42f9
Luis Cáceres <lacc97@protonmail.ch>
2024-09-05 23:12:23 +0100

    Add more convenient cmp config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,7 +4,46 @@ return {
     dependencies = { "hrsh7th/cmp-emoji" },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
+      -- Setup cmp-emoji as a cmp source
       table.insert(opts.sources, { name = "emoji" })
+
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local cmp = require("cmp")
+
+      -- Sensible completion key mapping and defaults
+      -- - Use Tab and S-Tab to select between completion options
+      -- - Use Enter to select and apply
+      -- - No default selection so that Enter works as normal if Tab and S-Tab haven't been pressed
+      -- - TODO: how do i deal with going back and trying another seletion with Tab/S-Tab?
+      opts.completion = {
+        completeopt = "noselect",
+      }
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<CR>"] = cmp.mapping(cmp.mapping.confirm({ select = false }), { "i", "s" }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
     end,
   },
 }
