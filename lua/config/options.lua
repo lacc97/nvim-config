64ba70f9a99272069242b767cda8779ef92b55a9
Luis Cáceres <lacc97@protonmail.ch>
2026-09-20 19:27:39 +0100

    Add guideline at 100 columns

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Disable animations
 vim.g.snacks_animate = false
+
+-- Guideline at 100 columns
+vim.api.nvim_create_autocmd("BufEnter", {
+  command = "setlocal colorcolumn=100",
+})
